e19a93af2de9d2cbf7a68cbcbee9688b5924a1f9
Adam Carruthers <adamadamadamc@gmail.com>
2017-01-20 09:36:58 -0600

    Updated ChatterBotAPI & several Nuget packages.

--- TestBot/Properties/Settings.Designer.cs
+++ TestBot/Properties/Settings.Designer.cs
@@ -37,7 +37,7 @@ namespace SoftwareBot.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("01/03/2017 15:20:00")]
+        [global::System.Configuration.DefaultSettingValueAttribute("01/20/2017 09:26:00")]
         public global::System.DateTime BUILD_DATE {
             get {
                 return ((global::System.DateTime)(this["BUILD_DATE"]));
